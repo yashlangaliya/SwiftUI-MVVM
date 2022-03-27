8dceecb0edcc539eac7e5281c6447a4896deec2d
yashlangaliya <yashlangaliya@gmail.com>
2022-03-27 11:43:25 +0530

    Initial Commit

---
+++ SwiftUI-MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI-MVVM
+//
+//  Created by Yash Langaliya on 27/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
